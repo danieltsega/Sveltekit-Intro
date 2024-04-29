88ea8bc6555299654c36665887bf471e14404e05
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-04-29 18:43:06 +0300

    [Feature]: Add the footer to the layout

--- company-site/src/routes/+layout.svelte
+++ company-site/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
     import '../app.css';
     import NavBar from "../components/NavBar.svelte"
+    import Footer from "../components/Footer.svelte"
 </script>
 
 <main>
     <NavBar/>
     <slot />
+    <Footer/>
 </main>
