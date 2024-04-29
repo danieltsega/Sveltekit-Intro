60ce5c74b62c689c92e77ed858621cf56bf992d5
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-04-29 16:58:19 +0300

    [Data]: Made a sample products data

---
+++ company-site/src/routes/product/data.js
@@ -0,0 +1,33 @@
+export const products = [
+  {
+    slug: "Lemon",
+    name: "Lemon-Juice",
+    price: 100,
+    description:
+      "This is a product of lemon juice, mixed with natural flavors.",
+  },
+
+  {
+    slug: "Orange",
+    name: "Orange-Juice",
+    price: 70,
+    description:
+      "This is a product of orange juice, mixed with natural flavors.",
+  },
+
+  {
+    slug: "Apple",
+    name: "Apple-Juice",
+    price: 150,
+    description:
+      "This is a product of apple juice, mixed with natural flavors.",
+  },
+
+  {
+    slug: "Mango",
+    name: "Mango-Juice",
+    price: 200,
+    description:
+      "This is a product of mango juice, mixed with natural flavors.",
+  },
+];
