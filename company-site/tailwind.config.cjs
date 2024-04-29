8bb68fdce44252f49276c05e8192c9de8f592e25
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-04-29 18:43:44 +0300

    [Config]: Add tailwind config file

---
+++ company-site/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.svelte", "./src/**/*.css"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
