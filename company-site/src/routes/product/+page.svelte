25226da94ba607506fc11b6eeaa8b3b7cb84bf7c
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-04-30 20:30:18 +0300

    [Feature]: Add styling to the products page

--- company-site/src/routes/product/+page.svelte
+++ company-site/src/routes/product/+page.svelte
@@ -2,10 +2,17 @@
     export let data;
 </script>
 
-<h1>Our Products Page</h1>
 
-<ul>
+<div class="flex flex-col items-center justify-center gap-5 text-center w-full py-8">
+
+    <h1 class="text-4xl font-bold">Our Products</h1>
+
+<ul class="flex flex-col gap-4">
     {#each data.details as { slug, name } }
-        <li><a href="/product/{slug}">{name}</a></li>        
+        <div class="p-4 rounded-md shadow-md flex justify-start bg-white">
+            <li><a href="/product/{slug}">{name}</a></li> 
+        </div>       
     {/each}
 </ul>
+
+</div>
