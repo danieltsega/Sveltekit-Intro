42f11bf34952b6c148a3b8396ef813b49892e945
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-04-30 20:38:09 +0300

    [Feature]: More styling on products page

--- company-site/src/routes/product/+page.svelte
+++ company-site/src/routes/product/+page.svelte
@@ -9,8 +9,9 @@
 
 <ul class="flex flex-col gap-4">
     {#each data.details as { slug, name } }
-        <div class="p-4 rounded-md shadow-md flex justify-start bg-white">
-            <li><a href="/product/{slug}">{name}</a></li> 
+        <div class="p-4 rounded-md shadow-md flex justify-start bg-white items-center gap-2 border hover:border-green-300 hover:ease-in-out hover:border-2">
+            <div class="w-8 h-8 bg-gray-400 rounded-md"></div>
+            <li><a class="text-lg font-semibold uppercase" href="/product/{slug}">{name}</a></li> 
         </div>       
     {/each}
 </ul>
