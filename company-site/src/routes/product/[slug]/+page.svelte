57870416be4b6cefc585d68a14509bcfe707e9f2
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-04-30 20:54:39 +0300

    [Feature]: Add styling to products detail page

--- company-site/src/routes/product/[slug]/+page.svelte
+++ company-site/src/routes/product/[slug]/+page.svelte
@@ -2,7 +2,11 @@
     export let data;
 </script>
 
-<h1>Product Detail</h1>
-<h2>{data.product.name}</h2>
-<p>Price: {data.product.price}</p>
-<p>Description: {data.product.description}</p>
+<div class="px-28 flex flex-col gap-4">
+    <h1 class="text-4xl font-bold">Product Detail</h1>
+    <div class="w-32 h-32 rounded-md bg-gray-400"></div>
+    <h2 class="text-lg font-semibold uppercase">{data.product.name}</h2>
+    <p class="text-lg uppercase font-semibold">Price: <span class="font-bold">$</span><span class="font-bold">{data.product.price}</span></p>
+    <p class="text-lg font-medium text-gray-500">{data.product.description}</p>
+    <button class="bg-pink-500 text-white font-semibold w-1/4 text-lg py-2 px-4 rounded-sm border-none hover:border-2 hover:border-pink-200 hover:bg-pink-600">BUY NOW</button>
+</div>
